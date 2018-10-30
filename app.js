99c29552317f17b0b94ebdd79f0b0bdf9a3937f9
JKasyan <j.kasyan86@gmail.com>
2018-10-31 01:14:26 +0200

    jwt

--- app.js
+++ app.js
@@ -1,63 +1,60 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+let express = require('express');
+let logger = require('morgan');
+let port = process.env.PORT || 3000;
+let secret = process.env.SECRET || 'SECRET_KEY';
+let bodyParser = require('body-parser');
+let router = express.Router();
+let app = express();
+let jwt = require('jsonwebtoken');
 
-passport.use(new LocalStrategy(
-    function(username, password, done) {
-        console.log('auth');
-        if (username === 'admin' && password === 'admin') {
-            return done(null, {username: 'admin'});
-        } else {
-            return done(null, false);
-        }
-    }
-));
-
-passport.serializeUser(function(user, done) {
-    done(null, user);
-});
-
-passport.deserializeUser(function(user, done) {
-    done(null, user);
-});
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-app.set('view engine', 'html');
-app.engine('html', require('ejs').renderFile);
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', __dirname + '/public/views');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(function(req, res, next) {
-    console.log('use ----->>>>>');
-    next();
-});
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-app.get('/login', function(req, res, next) {
-    res.render('login');
-});
-app.post('/login',
-    passport.authenticate('local', { failureRedirect: '/login' }),
-    function(req, res) {
-        console.log('redirect login');
-        res.redirect('/users');
+router.post('/authenticate', function(req, res, next) {
+    let username = req.body.username;
+    let password = req.body.password;
+    if(username === 'admin' && password === 'admin') {
+        const payload = {
+            username: 'admin'
+        };
+        let token = jwt.sign(payload, secret, {
+            expiresIn: "300s"
+        });
+        res.json({
+            success: true,
+            message: 'Enjoy your token!',
+            token: token
+        });
     }
-);
+});
+
+router.use(function (req, res, next) {
+    let token = req.body.token || req.query.token || req.headers['x-access-token'];
+    if (token) {
+        jwt.verify(token, secret, function(err, decoded) {
+            if(err) {
+                return res.status(403).send(err);
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        })
+    } else {
+        return res.status(403).send({message: "Not authorized"});
+    }
+});
+
+router.get('/data', function(req, res, next) {
+    res.send({message: "Some useful data"});
+});
+
+router.get('/', function(req, res, next) {
+    res.send({message: "Hello world!"});
+});
+
+app.use('/api', router);
+
+app.listen(port);
 
 module.exports = app;
